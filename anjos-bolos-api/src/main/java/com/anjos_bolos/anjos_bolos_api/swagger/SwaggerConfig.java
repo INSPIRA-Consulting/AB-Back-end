6b81ae9d67be193cf6d80127bb3562656ec6a6d8
LucasLucena31 <lucas.lucena@sptech.school>
2025-04-11 10:17:44 -0300

    Adição do swagger

---
+++ anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/swagger/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.anjos_bolos.anjos_bolos_api.swagger;
+
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiOAuthProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springdoc.webmvc.ui.SwaggerIndexTransformer;
+import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public SwaggerIndexTransformer swaggerIndexTransformer(
+            SwaggerUiConfigProperties swaggerUiConfig,
+            SwaggerUiOAuthProperties swaggerUiOAuthProperties,
+            ObjectMapperProvider objectMapperProvider,
+            SwaggerWelcomeCommon swaggerWelcomeCommon,
+            SwaggerUiConfigParameters swaggerUiConfigParameters) {
+
+        return new SwaggerCustomCssInjector(
+                swaggerUiConfig,
+                swaggerUiOAuthProperties,
+                swaggerWelcomeCommon,
+                swaggerUiConfigParameters,
+                objectMapperProvider
+        );
+    }
+}
