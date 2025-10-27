47263ef92730e64a0e703c62b4149ec6ce2fd1ee
LucasLucena31 <lucas.lucena@sptech.school>
2025-10-27 18:32:20 -0300

    fix: Ajuste de email a ser enviado

--- rabbit/inspira/inspira/src/main/java/school/sptech/bingo/consumer/BoloConsumer.java
+++ rabbit/inspira/inspira/src/main/java/school/sptech/bingo/consumer/BoloConsumer.java
@@ -18,13 +18,13 @@ public class BoloConsumer {
 
     @RabbitListener(queues = "${broker.queue.name}")
     public void consumirMensagem(BoloMensagem bolo) {
-        System.out.println("📦 Recebido da fila: " + bolo);
+        System.out.println("Recebido da fila: " + bolo);
 
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom("lucaslucena310805@gmail.com"); // precisa ser o mesmo do spring.mail.username
-            message.setTo("rickenschar@gmail.com"); // pra onde vai o email
-            message.setSubject("Novo pedido de bolo 🍰");
+            message.setTo("gustavo.spresilli@sptech.school"); // pra onde vai o email
+            message.setSubject("Novo pedido de bolo");
             message.setText(
                     "Detalhes do pedido:\n\n" +
                             "Peso: " + bolo.getPeso() + " kg\n" +
@@ -35,10 +35,10 @@ public class BoloConsumer {
             );
 
             mailSender.send(message);
-            System.out.println("📨 E-mail enviado com sucesso!");
+            System.out.println("E-mail enviado com sucesso!");
 
         } catch (Exception e) {
-            System.err.println("❌ Erro ao enviar e-mail: " + e.getMessage());
+            System.err.println("Erro ao enviar e-mail: " + e.getMessage());
         }
     }
 }
