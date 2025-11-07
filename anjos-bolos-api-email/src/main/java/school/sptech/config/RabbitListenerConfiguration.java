89d72d5ccf1896df9d2152732868272afc5d5ac4
GustavoPresilli <gustavo.spresilli@sptech.school>
2025-11-07 19:42:39 -0300

    TEstando alterações de Rabbit

---
+++ anjos-bolos-api-email/src/main/java/school/sptech/config/RabbitListenerConfiguration.java
@@ -0,0 +1,21 @@
+package school.sptech.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitListenerConfiguration {
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+}
