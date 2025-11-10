210d246a1946d51c5ae5473082fc8a03764a24ae
m-de-mello <murillo.mello@sptech.school>
2025-11-10 17:50:07 -0300

    Adicionando DTO de Feriados
    Adicionando DTO ausente no Commit anterior.

---
+++ anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/infrastructure/persistence/jpa/dto/feriados/FeriadosResponseDTO.java
@@ -0,0 +1,15 @@
+package com.anjos_bolos.anjos_bolos_api.infrastructure.persistence.jpa.dto.feriados;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public record FeriadosResponseDTO(
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        LocalDate data,
+        String nome,
+        String tipo,
+        String descricao,
+        String uf,
+        String municipio
+) {}
