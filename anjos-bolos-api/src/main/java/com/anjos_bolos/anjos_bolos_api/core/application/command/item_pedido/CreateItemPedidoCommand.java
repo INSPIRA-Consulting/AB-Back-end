6a2ee90e300a64c3d63340cd46898cc14d63ee3c
m-de-mello <murillo.mello@sptech.school>
2025-11-13 17:35:02 -0300

    Corrigindo Erro para a ItemPedidoRequestDTO
    Corrigindo erro para caso não seja enviado um valor para o Produto  em ItemPedidoRequestDTO.

--- anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/core/application/command/item_pedido/CreateItemPedidoCommand.java
+++ anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/core/application/command/item_pedido/CreateItemPedidoCommand.java
@@ -6,4 +6,13 @@ public record CreateItemPedidoCommand(
         Double precoUnitario,
         Integer quantidade,
         Double peso
-) {}
+) {
+
+    public CreateItemPedidoCommand(Integer pedidoId, Integer produtoId, Double precoUnitario, Integer quantidade, Double peso) {
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.precoUnitario = precoUnitario != null ? precoUnitario : 0.0;
+        this.quantidade = quantidade;
+        this.peso = peso;
+    }
+}
--- anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/core/application/command/item_pedido/UpdateItemPedidoCommand.java
+++ anjos-bolos-api/src/main/java/com/anjos_bolos/anjos_bolos_api/core/application/command/item_pedido/UpdateItemPedidoCommand.java
@@ -7,4 +7,14 @@ public record UpdateItemPedidoCommand(
         Double precoUnitario,
         Integer quantidade,
         Double peso
-) {}
+) {
+
+    public UpdateItemPedidoCommand(Integer id, Integer pedidoId, Integer produtoId, Double precoUnitario, Integer quantidade, Double peso) {
+        this.id = id;
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.precoUnitario = precoUnitario != null ? precoUnitario : 0.0;
+        this.quantidade = quantidade;
+        this.peso = peso;
+    }
+}
